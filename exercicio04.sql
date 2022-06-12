618cfe75965c54f5520fecfc59e54bf3b8991a59
Nathan Andrade <nathan.nhdsk@gmail.com>
2022-06-11 21:54:32 -0300

    adiciona exercicio 04 em relaX

--- exercicio04.sql
+++ exercicio04.sql
@@ -6,3 +6,10 @@ SELECT Projnumero, Dnum, Dnr, Unome, Endereco, Datanasc FROM Projeto
 WHERE Projeto.projlocal = 'Maua' 
 JOIN Departamento ON Derpartamento.Dnumero = Projeto.Dnum
 JOIN Funcionario ON Funcionario.Cpf = Departamento.Cpf_gerente; 
+
+/*
+Utilizando o RelaX para solução acima com CROSS JOIN
+*/
+πProjnumero, Dnum, Unome 
+σProjlocal = 'Maua' ∧ Dnum = Dnumero ∧ Cpf_gerente = Cpf 
+(Projeto ⨯ Departamento ⨯ Funcionario)
